9935fbf08ff44e395be85d26e4664e7d01aa5196
AlexWright55 <AlexandrAbramenko003@yandex.ru>
2026-05-25 02:39:51 +0600

    изменение кодировки

--- lib/ui/first_setup.lua
+++ lib/ui/first_setup.lua
@@ -17,7 +17,7 @@ local function EnsureInit()
     M.nick = imgui.new.char[64]("")
     M.name_surname = imgui.new.char[64]("")
     M.sex = imgui.new.int(0)
-    fractions = { u8("Р¤РџРЎ"), u8("РђСЂРјРёСЏ"), u8("Р”СЂСѓРіРѕРµ") }
+    fractions = { u8("ФПС"), u8("Армия"), u8("Другое") }
 end
 
 function M.Show()
@@ -29,40 +29,40 @@ end
 function M.Draw()
     EnsureInit()
     imgui.SetNextWindowSize(imgui.ImVec2(520, 400), imgui.Cond.Always)
-    imgui.Begin(u8("РџРµСЂРІРѕРЅР°С‡Р°Р»СЊРЅР°СЏ РЅР°СЃС‚СЂРѕР№РєР° Defency Helper"), M.Window, imgui.WindowFlags.NoResize)
+    imgui.Begin(u8("Первоначальная настройка Defency Helper"), M.Window, imgui.WindowFlags.NoResize)
 
-    imgui.TextColored(imgui.ImVec4(0.3, 0.8, 1.0, 1.0), u8("Р”РѕР±СЂРѕ РїРѕР¶Р°Р»РѕРІР°С‚СЊ! Р”Р°РІР°Р№С‚Рµ РЅР°СЃС‚СЂРѕРёРј СЃРєСЂРёРїС‚ РїРѕРґ РІР°СЃ."))
+    imgui.TextColored(imgui.ImVec4(0.3, 0.8, 1.0, 1.0), u8("Добро пожаловать! Давайте настроим скрипт под вас."))
     imgui.Separator()
 
     if M.step == 1 then
-        imgui.Text(u8("1. Р’С‹Р±РµСЂРёС‚Рµ РІР°С€Сѓ С„СЂР°РєС†РёСЋ:"))
+        imgui.Text(u8("1. Выберите вашу фракцию:"))
         imgui.Combo("##frac", M.fraction, fractions)
 
-        if imgui.Button(u8("Р”Р°Р»РµРµ"), imgui.ImVec2(-1, 40)) then
+        if imgui.Button(u8("Далее"), imgui.ImVec2(-1, 40)) then
             M.step = 2
         end
 
     elseif M.step == 2 then
-        imgui.Text(u8("2. Р’Р°С€Рё РґР°РЅРЅС‹Рµ:"))
-        imgui.InputText(u8("РќРёРє"), M.nick, 64)
-        imgui.InputText(u8("РРјСЏ Р¤Р°РјРёР»РёСЏ (RP)"), M.name_surname, 64)
+        imgui.Text(u8("2. Ваши данные:"))
+        imgui.InputText(u8("Ник"), M.nick, 64)
+        imgui.InputText(u8("Имя Фамилия (RP)"), M.name_surname, 64)
 
-        imgui.RadioButton(u8("РњСѓР¶СЃРєРѕР№ РїРµСЂСЃРѕРЅР°Р¶"), M.sex, 0)
+        imgui.RadioButton(u8("Мужской персонаж"), M.sex, 0)
         imgui.SameLine()
-        imgui.RadioButton(u8("Р–РµРЅСЃРєРёР№ РїРµСЂСЃРѕРЅР°Р¶"), M.sex, 1)
+        imgui.RadioButton(u8("Женский персонаж"), M.sex, 1)
 
-        if imgui.Button(u8("РќР°Р·Р°Рґ"), imgui.ImVec2(150, 40)) then
+        if imgui.Button(u8("Назад"), imgui.ImVec2(150, 40)) then
             M.step = 1
         end
         imgui.SameLine()
-        if imgui.Button(u8("РЎРѕС…СЂР°РЅРёС‚СЊ РЅР°СЃС‚СЂРѕР№РєРё"), imgui.ImVec2(-1, 40)) then
+        if imgui.Button(u8("Сохранить настройки"), imgui.ImVec2(-1, 40)) then
             if Defency.Config then
                 local nick = u8:decode(ffi.string(M.nick))
                 local name = u8:decode(ffi.string(M.name_surname))
 
                 Defency.Config.settings.player_info.nick = nick
                 Defency.Config.settings.player_info.name_surname = name
-                Defency.Config.settings.player_info.sex = (M.sex[0] == 0) and "РњСѓР¶СЃРєРѕР№" or "Р–РµРЅСЃРєРёР№"
+                Defency.Config.settings.player_info.sex = (M.sex[0] == 0) and "Мужской" or "Женский"
 
                 if M.fraction[0] == 0 then
                     Defency.Config.settings.general.fraction_mode = "prison"
@@ -76,7 +76,7 @@ function M.Draw()
             end
 
             M.Window[0] = false
-            sampAddChatMessage("{00FF00}[Defency] {FFFFFF}РќР°СЃС‚СЂРѕР№РєРё СЃРѕС…СЂР°РЅРµРЅС‹! РЎРєСЂРёРїС‚ РіРѕС‚РѕРІ Рє СЂР°Р±РѕС‚Рµ.", -1)
+            sampAddChatMessage("{00FF00}[Defency] {FFFFFF}Настройки сохранены! Скрипт готов к работе.", -1)
         end
     end
 
